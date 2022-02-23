5eb4431d42feeea664d1b0d6069ea70659ce85c8
Robert <robertmelveen.ndzonghan@ynov.com>
2022-02-23 13:03:07 +0100

    nom artiste test

---
+++ test_robert/main2.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Artist struct {
+	ID           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Locations    string   `json:"locations"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
+}
+
+func main() {
+	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var artistes []Artist
+	json.Unmarshal(responseData, &artistes)
+
+	for _, abc := range artistes {
+		fmt.Println(abc.Name)
+
+	}
+
+}

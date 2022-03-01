aef35aee8e8cc73ca53da31b3c8b0cf3c9a26ca2
Robert <robertmelveen.ndzonghan@ynov.com>
2022-03-01 09:30:26 +0100

    afficheartistterminal

--- test_robert/main2.go
+++ test_robert/main2.go
@@ -3,13 +3,14 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 )
 
-type Artist struct {
+type Artist []struct {
 	ID           int      `json:"id"`
 	Image        string   `json:"image"`
 	Name         string   `json:"name"`
@@ -20,8 +21,15 @@ type Artist struct {
 	ConcertDates string   `json:"concertDates"`
 	Relations    string   `json:"relations"`
 }
+type Data struct {
+	Name   string
+	Img    string
+	Global string
+	Id     int
+}
 
-func main() {
+func artist(w http.ResponseWriter, r *http.Request) {
+	template, _ := template.ParseFiles("test.html")
 	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
 
 	if err != nil {
@@ -34,12 +42,25 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var artistes []Artist
+	var artistes Artist
 	json.Unmarshal(responseData, &artistes)
 
-	for _, artist := range artistes {
-		fmt.Println(artist.Name)
-
+	for i := 0; i < len(artistes); i++ {
+		Api := Data{
+			Name: artistes[i].Name,
+			Img:  artistes[i].Image,
+			Id:   artistes[i].ID,
+		}
+		template.Execute(w, Api)
 	}
+}
 
+func main() {
+
+	css := http.FileServer(http.Dir("./css"))
+	http.Handle("/css/", http.StripPrefix("/css/", css))
+
+	http.HandleFunc("/", artist)
+
+	log.Fatal(http.ListenAndServe(":80", nil))
 }

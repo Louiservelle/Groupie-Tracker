1408e4b49a7c734ede1c02d5753ca81cdce3b9b0
Robert <robertmelveen.ndzonghan@ynov.com>
2022-03-01 09:35:12 +0100

    wrong code, modified

--- test_robert/main2.go
+++ test_robert/main2.go
@@ -1,66 +1,49 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"html/template"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "log"
+    "net/http"
+    "os"
 )
 
-type Artist []struct {
-	ID           int      `json:"id"`
-	Image        string   `json:"image"`
-	Name         string   `json:"name"`
-	Members      []string `json:"members"`
-	CreationDate int      `json:"creationDate"`
-	FirstAlbum   string   `json:"firstAlbum"`
-	Locations    string   `json:"locations"`
-	ConcertDates string   `json:"concertDates"`
-	Relations    string   `json:"relations"`
-}
-type Data struct {
-	Name   string
-	Img    string
-	Global string
-	Id     int
+type Artist struct {
+    ID           int      `json:"id"`
+    Image        string   `json:"image"`
+    Name         string   `json:"name"`
+    Members      []string `json:"members"`
+    CreationDate int      `json:"creationDate"`
+    FirstAlbum   string   `json:"firstAlbum"`
+    Locations    string   `json:"locations"`
+    ConcertDates string   `json:"concertDates"`
+    Relations    string   `json:"relations"`
 }
 
-func artist(w http.ResponseWriter, r *http.Request) {
-	template, _ := template.ParseFiles("test.html")
-	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
 
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
-	}
-
-	responseData, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var artistes Artist
-	json.Unmarshal(responseData, &artistes)
-
-	for i := 0; i < len(artistes); i++ {
-		Api := Data{
-			Name: artistes[i].Name,
-			Img:  artistes[i].Image,
-			Id:   artistes[i].ID,
-		}
-		template.Execute(w, Api)
-	}
-}
 
 func main() {
 
-	css := http.FileServer(http.Dir("./css"))
-	http.Handle("/css/", http.StripPrefix("/css/", css))
+    response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
 
-	http.HandleFunc("/", artist)
+    if err != nil {
+        fmt.Print(err.Error())
+        os.Exit(1)
+    }
 
-	log.Fatal(http.ListenAndServe(":80", nil))
-}
+    responseData, err := ioutil.ReadAll(response.Body)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    var artistes []Artist
+    json.Unmarshal(responseData, &artistes)
+
+    for _, artist := range artistes {
+        fmt.Println(artist.Name)
+
+    } 
+
+    }
+    
